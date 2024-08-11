6463975e1985d7c6ec7f706462755eb23798659a
Prensu <136965950+Prensu@users.noreply.github.com>
2024-08-11 07:56:55 +0545

    Create Hide&show.html

---
+++ ★彡[ᴊᴀᴠᴀꜱᴄɪʀᴘᴛ]彡★/Jquery/Hide&show.js
@@ -0,0 +1,21 @@
+<html> 
+<head></head>
+<body> 
+    <script src="https://code.Jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+  $(document).ready(function()
+  {
+    $("#hide").click(function(){
+      $("p").hide();
+    });
+    $("#show").click(function(){
+        $("p").show();
+    });
+  });
+</script>
+<h2>Jquery show and hide examples</h2>
+<p>Ascol is the most thotro college of nepal</p>
+<button id="hide">hide</button>
+<button id="show">show</button>
+</body>
+</html>
