6f74ec22f6d8f881677cdbf35d4e90449adcf218
Prensu <136965950+Prensu@users.noreply.github.com>
2025-04-14 07:50:50 +0545

    Happy Nepali New Year April 14th

---
+++  ➳لÅ𝓥Å➳/p10 Making my own Custom Package /MainApp.java
@@ -0,0 +1,10 @@
+import mypackage.MyClass;
+
+public class MainApp {
+    public static void main(String[] args) {
+        MyClass obj = new MyClass();
+        obj.displayMessage();
+    }
+}
+//literally i was struggling yesterday and now i am happy that its done !! A great way to start new year ! 
+// With forgetting past mistakes and taking new journey ahead ! 
